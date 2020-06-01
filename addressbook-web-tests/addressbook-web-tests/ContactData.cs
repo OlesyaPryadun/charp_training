6edeea8d035d765a1865f8d6dc1dddc107ac091a
Olesya Pradun <pryadun.olesya@gmail.com>
2020-06-01 10:46:42 +0300

    Task 4: Update for ContactData additional class

--- addressbook-web-tests/addressbook-web-tests/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace addressbook_web_tests
 {
-    class ContactData
+    public class ContactData
     {
+        private string firstName;
+        private string lastName;
+
+        
+        public ContactData (string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        
     }
 }
